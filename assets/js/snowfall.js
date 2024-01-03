a347ad128be322691f73c1d8d780c869ee904a99
ZiwiiDev <oliverfabian22@gmail.com>
2024-01-03 19:42:58 +0100

    Update contact

--- assets/js/snowfall.js
+++ assets/js/snowfall.js
@@ -7,20 +7,20 @@ document.addEventListener("DOMContentLoaded", function() {
     snowflake.style.left = Math.random() * window.innerWidth + "px";
     snowContainer.appendChild(snowflake);
 
-    // Añadir un retraso aleatorio para que no todos los copos de nieve comiencen al mismo tiempo
+    // Añado un retraso aleatorio para que no todos los copos de nieve comiencen al mismo tiempo
     setTimeout(() => {
       snowflake.style.animationDuration = Math.random() * 2 + 2 + "s";
       snowflake.style.opacity = Math.random();
     }, Math.random() * 2000);
 
-    // Eliminar el copo de nieve después de llegar al fondo
+    // Elimino el copo de nieve después de llegar al fondo
     snowflake.addEventListener("animationiteration", () => {
       snowContainer.removeChild(snowflake);
       createSnowflake();
     });
   }
 
-  // Crear copos de nieve
+  // Creo copos de nieve
   for (let i = 0; i < 35; i++) {
     createSnowflake();
   }
